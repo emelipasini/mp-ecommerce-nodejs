4b8bef3a74041ebaad84ab379fb59e83b715b4ca
emelipasini <emelipasini@outlook.com>
2020-08-13 17:54:01 -0300

    agregado integracion con mercado pago, falta terminar las rutas de redireccion

--- app.js
+++ app.js
@@ -1,17 +1,83 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
- 
+let mercadopago = require("mercadopago");
+mercadopago.configure(
+    {access_token: "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398"}
+);
+
 var app = express();
- 
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
-    res.render('home');
+    return res.render('home');
 });
 
 app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
+    return res.render('detail', req.query);
+});
+
+app.get("/success", function (req, res) {
+    return res.render("purchase", {query: req.query});
+});
+
+app.post("/notifications", function (req, res) {
+    return res.send(req.body);
+})
+
+app.post("/checkout", function (req, res) {
+    let preference = {
+        items: [
+            {
+                id: "1234",
+                title: req.body.title,
+                description: "Dispositivo móvil de Tienda e-commerce",
+                picture_url: req.body.img,
+                quantity: 1,
+                unit_price: parseInt(req.body.price)
+            }
+        ],
+        payer: {
+            name: "Lalo",
+            surname: "Landa",
+            email: "test_user_63274575@testuser.com",
+            phone: {
+                area_code: "11",
+                number: 22223333
+            },
+            address: {
+                zip_code: "1111",
+                street_name: "False",
+                street_number: 123
+            }
+
+        },
+        payment_methods: {
+            excluded_payment_methods: [
+                {id: "amex"}
+            ],
+            excluded_payment_types: [
+                {id: "atm"}
+            ],
+            installments: 6
+        },
+        back_urls: {
+            success: "https://emelipasini-mp-commerce-nodejs.herokuapp.com/success",
+            pending: "https://emelipasini-mp-commerce-nodejs.herokuapp.com/success",
+            failure: "https://emelipasini-mp-commerce-nodejs.herokuapp.com/success"
+        },
+        notification_url: "https://emelipasini-mp-commerce-nodejs.herokuapp.com/notifications",
+        auto_return: "approved",
+        external_reference: "emelipasini@gmail.com"
+    }
+    // res.send(preference);
+    mercadopago.preferences.create(preference)
+    .then(data => {
+        res.send(data);
+        return res.redirect(data.response.init_point);
+    }).catch(err => console.log(err));
 });
 
 app.use(express.static('assets'));
